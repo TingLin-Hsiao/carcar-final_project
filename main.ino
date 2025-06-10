a0ce442938f7d65a85b0ae564a2c6b4139e03c1e
chichigod <167602760+chichigod@users.noreply.github.com>
2025-06-10 16:21:42 +0800

    Add files via upload

---
+++ main.ino
@@ -0,0 +1,60 @@
+#include <Servo.h>
+Servo myServo;
+
+const int stepPin = 3;
+const int dirPin = 4;
+const int servo_pin = 9;
+
+const int return_steps = 10;
+const int nail_count = 40;  
+
+int nail_list[] = {};
+//0 8 16 24 16 24 32
+int angle_up = 100;                              //need to be measured
+int angle_down = 80;
+
+
+void setup() {
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+  myServo.attach(servo_pin);
+}
+
+void moveSteps(int steps, bool clockwise) {
+  digitalWrite(dirPin, clockwise ? HIGH : LOW);
+  for (int x = 0; x < steps; x++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500);
+  }
+}
+
+
+void loop() {
+  myServo.write(angle_up);
+  delay(3000);
+  int num = sizeof(nail_list) / sizeof(nail_list[0])-1;
+  for(int i=0; i<40; i++){
+    int distance =((nail_list[i+1] - nail_list[i]+1+nail_count)%nail_count)*2;
+    distance = 12*5*2;
+    moveSteps(distance, true);
+    delay(500); 
+    myServo.write(angle_down);
+    delay(500);
+
+    moveSteps(return_steps, false);
+    delay(500);
+    myServo.write(angle_up);
+    delay(500);
+
+
+    
+  }
+  delay(10000);
+}
+
+
+
+
+
